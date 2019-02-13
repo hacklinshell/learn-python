3f156ba293dd236766e9efa7407c62d7420aebcb
张琳 <hacklinshell@gmail.com>
2019-02-13 16:34:12 +0800

    字符串和编码

---
+++ python基础/learn.py
@@ -0,0 +1,14 @@
+
+print("hello")
+
+print("test")
+print("中文英文str")  #python3 字符串是Unicode编码
+
+ord('A')  #65
+ord('琳') #29747
+chr(77)   #'M'
+len('ABC') # 3
+len('中文') # 2  
+len('中文'.encode('utf-8')) #6
+str = 'world'
+print("hello,%s" % str )
